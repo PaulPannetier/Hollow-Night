5f43b0bcc614b94d2dec12a55c9560fae7cb4243
LeonardPannetier <leonard.pannetier@laposte.net>
2024-11-15 21:43:26 +0100

    l

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private InputManager.GeneralInput upInput;
+    [SerializeField] private InputManager.GeneralInput downInput;
+    [SerializeField] private InputManager.GeneralInput rightInput;
+    [SerializeField] private InputManager.GeneralInput leftInput;
+    [SerializeField] private InputManager.GeneralInput sprintInput;
+    [SerializeField] private InputManager.GeneralInput torchInput;
+
+    private ControllerType _controllerType;
+    private ControllerType controllerType
+    {
+        get => _controllerType;
+        set
+        {
+            _controllerType = value;
+            upInput.controllerType = value;
+            downInput.controllerType = value;
+            rightInput.controllerType = value;
+            leftInput.controllerType = value;
+            sprintInput.controllerType = value;
+            torchInput.controllerType = value;
+        }
+    }
+
+    public bool isSprintUp => sprintInput.IsPressedUp();
+    public bool isTorchUp => torchInput.IsPressedUp();
+    public bool isSprintDown => sprintInput.IsPressedDown();
+    public bool isTorchDown => torchInput.IsPressedDown();
+    public bool isSprintPressed => sprintInput.IsPressed();
+    public bool isTorchPressed => torchInput.IsPressed();
+
+    [HideInInspector] public float x, y;
+    [HideInInspector] public int rawX, rawY;
+
+    private void Update()
+    {
+        if(controllerType == ControllerType.Keyboard)
+        {
+            if (rightInput.IsPressed())
+            {
+                x = leftInput.IsPressed() ? 0f : 1f;
+            }
+            else
+            {
+                x = leftInput.IsPressed() ? -1f : 0f;
+            }
+
+            if (upInput.IsPressed())
+            {
+                y = downInput.IsPressed() ? 0f : 1f;
+            }
+            else
+            {
+                y = downInput.IsPressed() ? -1f : 0f;
+            }
+        }
+        else
+        {
+            Vector2 tmbStickPosition = InputManager.GetGamepadStickPosition(controllerType, GamepadStick.left).normalized;
+            x = Mathf.Clamp01(tmbStickPosition.x);
+            y = Mathf.Clamp01(tmbStickPosition.y);
+        }
+
+        rawX = Mathf.Abs(x) >= 0.1f ? (x > 0f ? 1 : - 1) : 0;
+        rawY = Mathf.Abs(y) >= 0.1f ? (y > 0f ? 1 : - 1) : 0;
+    }
+}
