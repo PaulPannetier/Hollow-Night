75f10c9b60ae7f11e8637ceb75ac31b5f7f42a25
SpoLunchkY <baptiste.reig@outlook.fr>
2024-11-15 21:23:34 +0100

    a

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,61 @@
+using System.Linq;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject[] players;
+
+    [SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private Transform endLevelUI;
+
+
+    private bool Error => players.Count() > spawnPoints.Count();
+
+
+    void Start()
+    {
+        if (Error)
+            Debug.LogWarning("pas assez de spawnPoints pour les joueurs");
+
+        InitialiseGame();
+    }
+
+    void Update()
+    {
+        if (CheckEndGame())
+            EndGame();
+
+    }
+
+    private void InstantiatePlayers()
+    {
+        for (int i = 0; i < players.Count(); i++)
+        {
+            Instantiate(players[i], spawnPoints[i].position, spawnPoints[i].rotation, transform);
+        }
+    }
+
+    private void InitialiseGame()
+    {
+        Time.timeScale = 1;
+        endLevelUI.gameObject.SetActive(false);
+
+        InstantiatePlayers();
+    }
+
+    private bool CheckEndGame()
+    {
+        if (players.Count() > 0)
+            return false;
+
+        return true;
+    }
+
+    private void EndGame()
+    {
+        Time.timeScale = 0;  
+        endLevelUI.gameObject.SetActive(true);
+    }
+
+
+}
