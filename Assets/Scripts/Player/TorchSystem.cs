232f98181a571f5ea828c794de39dd0ae320c40d
LeonardPannetier <leonard.pannetier@laposte.net>
2024-11-16 00:33:19 +0100

    l

--- Assets/Scripts/Player/TorchSystem.cs
+++ Assets/Scripts/Player/TorchSystem.cs
@@ -8,19 +8,17 @@ public class TorchSystem : MonoBehaviour
     {
         playerInput = GetComponent<PlayerInput>();
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         torchLight.enabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (playerInput.isTorchPressed)
         {
             torchLight.enabled = true;
-            Debug.Log("pressed");
         }
         else if (playerInput.isTorchUp)
         {
