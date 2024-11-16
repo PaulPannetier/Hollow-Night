73a764348c2494ddb54e8da1c445a094ccf133f0
LeonardPannetier <leonard.pannetier@laposte.net>
2024-11-16 04:02:28 +0100

    debut raycast

--- Assets/Scripts/Player/TorchSystem.cs
+++ Assets/Scripts/Player/TorchSystem.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TorchSystem : MonoBehaviour
@@ -6,10 +7,13 @@ public class TorchSystem : MonoBehaviour
     [SerializeField] private Light torchLight;
     [SerializeField] private Light spottedLight;
     private bool isNight => LevelManager.instance.isNight;
+    private LayerMask playerAndWallMask;
+    private bool test;
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
+        playerAndWallMask = LayerMask.GetMask("Map", "Player");
     }
 
     void Start()
@@ -24,13 +28,24 @@ public class TorchSystem : MonoBehaviour
         {
             torchLight.enabled = true;
             spottedLight.enabled = true;
+            RaycastHit hitInfo;
+            Physics.Raycast(transform.position, transform.forward, out hitInfo, 4f, playerAndWallMask);
+            Debug.DrawLine(transform.position, transform.position + 4*transform.forward, Color.green);
+            if (hitInfo.collider != null)
+            {
+                Debug.Log(hitInfo.rigidbody.gameObject);
+                if(hitInfo.rigidbody.gameObject != null)
+                {
+                    Debug.Log("touché");
+                    LevelManager.instance.DestroyPlayer(hitInfo.rigidbody.gameObject.GetComponent<PlayerData>());
+                }
+            }
         }
         else if (playerInput.isTorchUp)
         {
             torchLight.enabled = false;
             spottedLight.enabled = false;
         }
-
-
+        
     }
 }
